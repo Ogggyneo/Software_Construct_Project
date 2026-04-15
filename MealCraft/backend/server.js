a7a60be0b805c7acd6efcd72f8b325c09eeb1ee2
leoleo <qp713223@gmail.com>
2026-04-15 13:52:56 +0700

    Update server.js

--- MealCraft/backend/server.js
+++ MealCraft/backend/server.js
@@ -3,26 +3,18 @@ const cors = require('cors');
 
 const app = express();
 const PORT = 5000;
-
 app.use(cors());
 app.use(express.json());
-
-/* ---------------- ROUTES ---------------- */
-
 app.get('/api/test', (req, res) => {
   console.log("📥 GET /api/test hit");
 //   res.json({ message: 'Backend is working!' });
 });
-
-/* ---------------- CATCH ALL (LAST!) ---------------- */
-
 app.use((req, res) => {
   console.log(`❌ NOT HANDLED: ${req.method} ${req.url}`);
   res.status(404).json({
     message: "Route not found"
   });
 });
-
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
